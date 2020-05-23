af521e69f45d93c68fdffe042629e5e4df80f9b2
tomasarduino <tomasarduino4@gmail.com>
2020-05-24 00:12:06 +0200

    Agregando mas al commit

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route("/")   ##direccion de la ruta final##
+@app.route("/")  
 def index():
-	titulo = "Cauchos"  ##Definiendo variable##
-	lista = ["Marca","Dimiensiones","Precio","Disponibilidad"] ##Definiendo una lista
-	return render_template("index.html", titulo = titulo, lista = lista) ##Mostrando variable "titulo" en conjunto con indexx.html##
+	titulo = "Cauchos"  
+	lista = ["Marca","Dimiensiones","Precio","Disponibilidad"]
+	return render_template("index.html", titulo = titulo, lista = lista) 
+
+@app.route("/youtube")
+def go_to_youtube():
+	return redirect("https://www.youtube.com/")
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+	if request.method == "POST":
+		return redirect(url_for("/"))
+	return render_template("signup.html")
 
 if __name__ == "__main__":
 	app.run(debug=True, port=3000)
